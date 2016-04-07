5bf579249c94fe37471c3b3aaaacfdfd17b0fde9
Min Cai <min.cai.china@gmail.com>
2016-04-07 14:55:36 +0800

    Update analyze_gem5_alpha_parsec2.1.py.

--- run_gem5_alpha_parsec2.1.py
+++ run_gem5_alpha_parsec2.1.py
@@ -16,8 +16,8 @@ def run(bench, input_set, l2_size, l2_assoc, num_threads):
     os.system('rm -fr ' + dir)
     os.system('mkdir -p ' + dir)
 
-    cmd_run = '../gem5/build/ALPHA_MESI_Two_Level/gem5.opt -d ' + dir + ' configs/example/fs.py --two-phase --cpu-type=timing --num-cpus=' \
-              + str(num_threads) + ' --script=ext/parsec/2.1/run_scripts/' \
+    cmd_run = '../gem5/build/ALPHA_MESI_Two_Level/gem5.opt -d ' + dir + ' ../gem5/configs/example/fs.py --two-phase --cpu-type=timing --num-cpus=' \
+              + str(num_threads) + ' --script=../gem5_extras/parsec/2.1/run_scripts/' \
               + bench + '_' + str(num_threads) + 'c_' + input_set + '.rcS' \
               + ' --caches --l2cache --num-l2caches=1' \
               + ' --l1d_size=32kB --l1i_size=32kB --l2_size=' + l2_size + ' --l2_assoc=' + str(l2_assoc)
